0a51100b537b1ec8b472ef9accb902dc87d64e76
Justin Johnson <justin.johnson@smartbear.com>
2018-09-02 22:18:06 -0400

    SS + Client common routes created

---
+++ ServerSideReact/src/client/Routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+
+export default () => (
+  <div>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/hi" component={() => 'Hi'} />
+  </div>
+);
