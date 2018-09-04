90435dc80a46892ec13e045a83ab9ab0bda378bb
Justin Johnson <justin.johnson@smartbear.com>
2018-09-04 06:26:00 -0400

    fetch users action + reducer

---
+++ ServerSideReact/src/client/components/UsersList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+
+class UsersList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUsers = () => {
+    return this.props.users.map(user => (
+      <li key={user.id}>
+        {user.name}
+      </li>
+    ));
+  }
+
+  render() {
+    const { users } = this.props;
+    return (
+      <div>
+        List of users!
+        <ul>
+          {this.renderUsers()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ users }) => ({ users });
+
+const mapDispatchToProps = {
+  fetchUsers
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersList);
