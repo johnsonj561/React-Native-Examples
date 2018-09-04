1079e23a66288a7502a7b96112e5d4d975746ccc
Justin Johnson <justin.johnson@smartbear.com>
2018-09-04 06:25:40 -0400

    adding redux to client + server

--- ServerSideReact/src/index.js
+++ ServerSideReact/src/index.js
@@ -1,12 +1,17 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  const html = renderer(req);
+  const store = createStore();
+  // logic to init store / load data
+
+  const html = renderer(req, store);
   res.send(html);
 });
 
